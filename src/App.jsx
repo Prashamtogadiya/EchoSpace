a6bd52ca19b99888552ebd499f5f3b79776a2ffb
Prashamtogadiya <pktogadiya222@gmail.com>
2025-10-28 21:03:23 +0530

    Done set up of application

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full flex  h-screen justify-center items-center p-4'>
+      <div className='border-[1px] border-gray-700 max-w-6xl w-full min-h-[450px] rounded-lg'>
+        {/* Header */}
+        <div className='flex justify-between h-20 border-b-[1px] border-gray-700'>
+          <div className='p-4'>
+            <p className='text-gray-300'>Signed is as name..</p>
+            <p className='text-gray-300 italic text-sm'>3 users online </p>
+          </div>
+          <button className='m-2 sm:mr-4'>Sign out</button>
+        </div>
+        {/* main chat */}
+        <div></div>
+        {/* message input */}
+        <form className='flex flex-col sm:flex-row p-4 border-t-[1px] border-gray-700'>
+          <input type="text" placeholder='Type a message...' className='p-2 w-full bg-[#00000040] rounded-lg' />
+          <button className='mt-4 sm:mt-0 sm:ml-8 text-white max-h-12'>Send</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App
